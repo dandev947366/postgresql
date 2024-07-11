8c4ae9f20ece465150d4a8c4d281ac1ea3cc73d3
danofoundation <dan.dev947366@gmail.com>
2024-07-11 09:41:28 +0300

    Add connection

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello')
+import psycopg2 as ps
+
+connectDB = ps.connect(
+    dbname="studentdb",
+    user="postgres",
+    password="password",
+    host="localhost",
+    port="5432"
+)
+print(connectDB)
+
+
+
